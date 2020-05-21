1864d10c89d43819d25c636b359bff2165316611
Chinmay Bhushan <bhushan.chinmay@gmail.com>
2020-05-21 15:39:26 +0530

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlZEezf-nMEaGChvlL-fwsQL54CiDRos4",
+    authDomain: "newsy-606eb.firebaseapp.com",
+    databaseURL: "https://newsy-606eb.firebaseio.com",
+    projectId: "newsy-606eb",
+    storageBucket: "newsy-606eb.appspot.com",
+    messagingSenderId: "548037126617",
+    appId: "1:548037126617:web:0a97fc663be21ecdbe91c8"
+  };
+
+  export default firebaseConfig;
+  
