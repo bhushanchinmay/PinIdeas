7ad1ee69b21ac62d937d58fe62112da6ea4c3c82
bhushan.chinmay <21293962+bhushanchinmay@users.noreply.github.com>
2025-06-09 10:11:02 +0000

    build fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from react-dom/client
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root'); // Get the root element
+const root = ReactDOM.createRoot(container); // Create a root
+
+root.render( // Render the App component
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
